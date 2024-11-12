15f2ad5faeedf9a49088c0915c5a4750f1caab67
Pramod Nishad <nishadpramod.001@gmail.com>
2024-11-12 13:29:33 +0530

    conversion done

---
+++ 03_conversionOperations.js
@@ -0,0 +1,53 @@
+// let score  = 33
+let score  = "33abc"
+
+// console.log(typeof(score))
+
+let convertedNumber = Number(score)
+// console.log(typeof(convertedNumber))
+// console.log(convertedNumber)
+
+// 33 => 33 converted easily
+// "33abc" => Nan
+// true => 1; false =>2
+
+let loggedIn = 1
+let boolean = Boolean(loggedIn)
+// console.log(boolean)
+
+let someNumber = 10;
+let someNumberConvetrd = String(someNumber)
+// console.log(someNumber)
+// console.log(typeof(someNumberConvetrd))
+
+
+// ********************Operations**************
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(20%2)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+
+let num1, num2,num3
+num1 = num2 = num3 = 5+5;
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
+gameCounter++;
+console.log(gameCounter)
+
+
+
+
