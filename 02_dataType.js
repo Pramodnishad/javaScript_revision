3cd253df8b6ad14618c3478fdc03ec88f9c8b93c
Pramod Nishad <nishadpramod.001@gmail.com>
2024-11-12 11:24:47 +0530

    variables and datatype

---
+++ 02_dataType.js
@@ -0,0 +1,13 @@
+let name = "Pramod"  //string
+let age = 25 // number
+let isLoggedIn = false // boolean
+
+let state = null // null => stand alone value
+//  Symbol = unique
+//  Undefined =>
+//  Object
+//  TypeOf
+
+// console.table(typeof[name,age,isLoggedIn])
+// console.log(typeof state) ==> Object
+console.log(first)
